1e2261b43824400835e5435d63737339aaff15e1
khawaja-ammar <mammarkh@gmail.com>
2023-05-07 20:27:16 +0500

    Updates

---
+++ precache-manifest.3dbd7568e5840c697c267e992a45a91c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7adddb770365b9c8c37",
+    "url": "/reactResturant/static/css/main.5456f27b.chunk.css"
+  },
+  {
+    "revision": "b7adddb770365b9c8c37",
+    "url": "/reactResturant/static/js/main.b7adddb7.chunk.js"
+  },
+  {
+    "revision": "27301f88786ae1443910",
+    "url": "/reactResturant/static/js/1.27301f88.chunk.js"
+  },
+  {
+    "revision": "2ca2b03eead36f8abe3d",
+    "url": "/reactResturant/static/js/runtime~main.2ca2b03e.js"
+  },
+  {
+    "revision": "be2151f4e9af529f67ce80896ed8374a",
+    "url": "/reactResturant/static/media/meal.be2151f4.mp4"
+  },
+  {
+    "revision": "d719065bb2726afe77e90cec8ea81d5a",
+    "url": "/reactResturant/static/media/bg.d719065b.png"
+  },
+  {
+    "revision": "85d27f24ca6480fa5e10c95311f10e19",
+    "url": "/reactResturant/static/media/chef.85d27f24.png"
+  },
+  {
+    "revision": "c7fdecee851896549a8f719d1d792e75",
+    "url": "/reactResturant/static/media/G.c7fdecee.png"
+  },
+  {
+    "revision": "5a3891e8d95239f82c09715ae11941b6",
+    "url": "/reactResturant/static/media/gallery01.5a3891e8.png"
+  },
+  {
+    "revision": "2dd1c3366e4ba309e1ab5ef1c4d31f2d",
+    "url": "/reactResturant/static/media/gallery02.2dd1c336.png"
+  },
+  {
+    "revision": "3706b6f7f3577e20cbc4bec6e7698eb5",
+    "url": "/reactResturant/static/media/gallery03.3706b6f7.png"
+  },
+  {
+    "revision": "f51d9759a0055fffba51cb9949949771",
+    "url": "/reactResturant/static/media/gallery04.f51d9759.png"
+  },
+  {
+    "revision": "e96f9bd45b8e0b34eeb03e93f547ccf8",
+    "url": "/reactResturant/static/media/knife.e96f9bd4.png"
+  },
+  {
+    "revision": "5c00c519489e1238ee18cc42a47f141a",
+    "url": "/reactResturant/static/media/logo.5c00c519.png"
+  },
+  {
+    "revision": "cc54616224f2b073f9400318c172d8a6",
+    "url": "/reactResturant/static/media/menu.cc546162.png"
+  },
+  {
+    "revision": "179769474d7506d29c78a6bec834b00c",
+    "url": "/reactResturant/static/media/overlaybg.17976947.png"
+  },
+  {
+    "revision": "72422a1acc4f48e3d82ae3bfa6d1b9d8",
+    "url": "/reactResturant/static/media/spoon.72422a1a.svg"
+  },
+  {
+    "revision": "fd16528f71a845cc74618cf2bd3d8995",
+    "url": "/reactResturant/static/media/welcome.fd16528f.png"
+  },
+  {
+    "revision": "03ef4140ada413449ce1d79046217392",
+    "url": "/reactResturant/static/media/findus.03ef4140.png"
+  },
+  {
+    "revision": "c2d7d58a629c456aac6406b8986272fb",
+    "url": "/reactResturant/static/media/laurels.c2d7d58a.png"
+  },
+  {
+    "revision": "a807ceab88095e0cbece581d1600f52d",
+    "url": "/reactResturant/static/media/award01.a807ceab.png"
+  },
+  {
+    "revision": "ff49ca57a8d44a4596c62d4966eb5788",
+    "url": "/reactResturant/static/media/award02.ff49ca57.png"
+  },
+  {
+    "revision": "73a36a9eac74bab796a0e1c799a7ad4a",
+    "url": "/reactResturant/static/media/award03.73a36a9e.png"
+  },
+  {
+    "revision": "d13582bfb80458cf3abd2268d5b5b581",
+    "url": "/reactResturant/static/media/award05.d13582bf.png"
+  },
+  {
+    "revision": "f1363e1deeb3ee759b3a365913a97c85",
+    "url": "/reactResturant/static/media/sign.f1363e1d.png"
+  },
+  {
+    "revision": "1aa54beb69da36e7928f1d070bba3c59",
+    "url": "/reactResturant/index.html"
+  }
+];
